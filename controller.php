fca43d73d7e075fc27f406166501609b207ff1a0
sakamotosora0116 <92964241+sakamotosora0116@users.noreply.github.com>
2022-10-20 03:39:06 -0700

    Create controller.php

---
+++ controller.php
@@ -0,0 +1,46 @@
+   public function upto(Post $post)
+    {
+        $num = $post->pos;
+
+        $target = DB::table('posts')->where('post_id', '>', $num)->min('post_id');
+
+        if ($target == NULL)
+        {
+            return redirect()
+                ->route('posts.index');
+        }
+        DB::table('posts')->where('pos', '=', $target)->update(['pos' => $num]);
+        DB::table('posts')->where('id', '=', $post->id)->update(['pos' => $target]);
+        return redirect()
+            ->route('posts.index');
+    }
+
+    public function downTo(Post $post)
+    {
+        $num = $post->pos;
+
+        $target = DB::table('posts')->where('pos', '<', $num)->max('pos');
+
+        if ($target == NULL)
+        {
+            return redirect()
+                ->route('posts.index');
+        }
+        DB::table('posts')->where('pos', '=', $target)->update(['pos' => $num]);
+        DB::table('posts')->where('id', '=', $post->id)->update(['pos' => $target]);
+        return redirect()
+            ->route('posts.index');
+    }
+          
+          
+    public function checked(Post $post)
+    {
+
+        $post->is_done = !$post->is_done;
+
+        $post->save();
+
+        return redirect()
+            ->route('posts.index');
+
+    }
