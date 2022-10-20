03beb8f8e85d5f8ae8e2c98d6787b642c2111843
sakamotosora0116 <92964241+sakamotosora0116@users.noreply.github.com>
2022-10-19 23:30:08 -0700

    Create index.blade.php

---
+++ index.blade.php
@@ -0,0 +1,8 @@
+        <div class="ope-container">
+            <span class="upChange change" data-id="{{ $post->id }}">⇧UP</span>
+            <span class="downChange change" data-id="{{ $post->id }}">⇩DWN</span>
+            <span class="topChange change" data-id="{{ $post->id }}">  ⇧TOP</span>
+            <span class="downChange change">⇩BTM</span>
+        </div>
+        
+        {{ --- upto downto ---}}
