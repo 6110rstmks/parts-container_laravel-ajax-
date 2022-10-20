e6c7bd03af10d084b886674073a71755157593de
sakamotosora0116 <92964241+sakamotosora0116@users.noreply.github.com>
2022-10-19 23:27:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ checkboxes.forEach(checkbox => {
 });
 
 // checkbox 
+
+/*upto*/
+uptos.forEach(upto => {
+    const postid = upto.dataset.id;
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    upto.addEventListener('click', () => {
+
+        fetch('/posts/'+ postid + '/upto', {
+            method: 'PATCH',
+            headers: {
+                'X-CSRF-Token': token,
+            }
+        });
+
+        const preTarget = upto.parentNode.parentNode.previousElementSibling;
+        const moveTarget = upto.parentNode.parentNode;
+
+        if (moveTarget !== ul.children[0]) {
+          ul.insertBefore(moveTarget, preTarget);
+        }
+    })
+});
